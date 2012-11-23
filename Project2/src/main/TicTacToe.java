ab81777d281caf0153fafb4a767acf0eb188921c
Fridrik Laxdal Karason <friggilax@gmail.com>
2012-11-23 17:32:14 +0000

    First created the test DefaultConstructorTest and then created a default constructor for the class TicTacToe

--- Project2/src/main/TicTacToe.java
+++ Project2/src/main/TicTacToe.java
@@ -3,10 +3,20 @@ package main;
 public class TicTacToe
 {
     public boolean quitGame = false;
-
+    public char[][] arr;
     public TicTacToe()
     {
         this.quitGame = false;
+        arr=new char[3][3];
+        char cntr='1';
+        for (int i=0; i<3; i++)
+        {
+             for (int j=0;j<3;j++)
+             {
+                 arr[i][j]=cntr;
+                 cntr++;
+             }
+        }
     }
 
     public boolean isQuitGame()
